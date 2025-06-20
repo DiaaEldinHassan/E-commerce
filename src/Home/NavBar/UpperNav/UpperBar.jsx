e446cf9f2180a13942138a693438f42620a7e8df
DiaaEldinHassan <diaahassan777@gmail.com>
2025-06-20 18:20:43 +0300

    Editing directory

--- src/Home/NavBar/UpperNav/UpperBar.jsx
+++ src/Home/NavBar/UpperNav/UpperBar.jsx
@@ -1,6 +1,7 @@
 import React from "react";
 import "./UpperBar.css";
 import { about } from "../arrays";
+import { Link } from "react-router-dom";
 
 export default function UpperBar()
 {
@@ -8,7 +9,7 @@ export default function UpperBar()
         <div className="upperBar">
             {about.map((i,index)=>{
                 return(
-                    <a href={i.href} className="text-sm" key={index}>{i.name}</a>
+                    <Link to={i.href} className="text-sm" key={index}>{i.name}</Link>
                 )
             })}
         </div>
